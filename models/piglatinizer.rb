ef896328ad714902a73cd6116166624b3d0cd124
Michelle Moon <michellemoon236@gmail.com>
2019-05-31 05:32:07 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -11,16 +11,16 @@ class PigLatinizer
   end
   
   def piglatinize_word(word)
-    first_letter = word[0].downcase
-    second_letter = word[1].downcase if word[1]
-    third_letter = word[2].downcase if word[2]
+    first_letter = word[0]
+    second_letter = word[1] if word[1]
+    third_letter = word[2] if word[2]
     
     #inding.pry
-    if first_letter.scan(/[aeiou]/).first
+    if first_letter.downcase.scan(/[aeiou]/).first
       "#{word}way"
       #starts with consonants below
-    elsif second_letter.scan(/[bcdfghjklmnpqrstvwxyz]/).first
-      if third_letter.scan(/[bcdfghjklmnpqrstvwxyz]/).first #first, second, third letters are consonants
+    elsif second_letter.downcase.scan(/[bcdfghjklmnpqrstvwxyz]/).first
+      if third_letter.downcase.scan(/[bcdfghjklmnpqrstvwxyz]/).first #first, second, third letters are consonants
         word[3..-1]+first_letter+second_letter+third_letter+"ay"        
       else #first and second letter consonants
         word[2..-1]+first_letter+second_letter+"ay"
